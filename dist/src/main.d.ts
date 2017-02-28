512fd9eb2d9b1fa8ab8e0948a33c95bd7bde1d2f
Pavel Petrochenko <petrochenko.pavel.a@gmail.com>
2017-02-28 11:56:22 +0600

    removing dist folder from git

--- dist/src/main.d.ts
+++
@@ -1,9 +0,0 @@
-import rp = require("raml-1-parser");
-export interface ProcessingResult {
-    ramlSource: string;
-    types: {
-        [name: string]: any;
-    };
-}
-export declare function process(x: rp.api10.LibraryBase): ProcessingResult;
-export declare function parseToJSON(url: string, f: (v: ProcessingResult) => void): void;
